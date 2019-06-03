9375daeeafcc2b2975f5c8398636493d63c3168e
LittleYenMin <fkym08@gmail.com>
2019-06-03 15:58:08 +0800

    First commit.

---
+++ Chapter2 Basic sequential model.py
@@ -0,0 +1,30 @@
+import keras
+
+# Load MNIST dataset from keras
+(train_images, train_labels), (test_images, test_labels) = keras.datasets.mnist.load_data()
+# create Sequential model
+network = keras.models.Sequential()
+# add 2 Dense Layer to the model
+network.add(keras.layers.Dense(512, activation='relu', input_shape=(28*28,)))
+network.add(keras.layers.Dense(10, activation='softmax'))
+# compile model by optimizer, loss and metrics
+network.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# prepare data
+train_images = train_images.reshape((60000, 28*28))
+train_images = train_images.astype('float32')/255
+
+test_images = test_images.reshape((10000, 28*28))
+test_images = test_images.astype('float32')/255
+
+# category the labels
+train_labels = keras.utils.to_categorical(train_labels)
+test_labels = keras.utils.to_categorical(test_labels)
+
+# train model by fit() method
+history = network.fit(train_images, train_labels, epochs=5, batch_size=128)
+print(history)
+
+# review the train result by evaluate() method and test datasets.
+test_loss, test_acc = network.evaluate(test_images, test_labels)
+print(test_loss, test_acc)
---
+++ Readme.md
@@ -0,0 +1,19 @@
+Notes for Deep Learning with Python
+=============================
+Implementation by the book **Deep Learning with Python**.
+
+Chapters
+----------
+ - [x] 2 開始之前：了解神經網路的數學概念
+ - [ ] 3 開始使用神經網路
+ - [ ] 4 機器學習的基礎知識
+ - [ ] 5 深度學習實務電腦視覺的深度學習
+ - [ ] 6 應用於文字資料與序列資料的深度學習 
+ - [ ] 7 進階深度學習的最佳實作方式
+ - [ ] 8 生成式深度學習
+ - [ ] 9 結語
+
+Ref
+--------
+ISBN：9781617294433
+![ISBN：9781617294433](readme/cover.jpg)

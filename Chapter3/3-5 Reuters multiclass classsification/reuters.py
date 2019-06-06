801c5cb4c8e3b195294b719b016aad6d4b52f4b2
LittleYenMin <fkym08@gmail.com>
2019-06-06 14:45:11 +0800

    Extract decode newswire to be a method.

--- Chapter3/3-5 Reuters multiclass classsification/reuters.py
+++ Chapter3/3-5 Reuters multiclass classsification/reuters.py
@@ -9,12 +9,15 @@ def one_hot_encoding(sequences, dimension=10000):
     return results
 
 
-(train_data, train_label), (test_data, test_label) = keras.datasets.reuters.load_data(num_words=10000)
-
 word_index = keras.datasets.reuters.get_word_index()
 reverse_word_index = dict([(value, key) for key, value in word_index.items()])
-decoded_newswire = ' '.join([reverse_word_index.get(i-3, '?') for i in train_data[0]])
-print(decoded_newswire)
+
+
+def decoded_newswire(sequences):
+    return ' '.join([reverse_word_index.get(i - 3, '?') for i in train_data[0]])
+
+
+(train_data, train_label), (test_data, test_label) = keras.datasets.reuters.load_data(num_words=10000)
 
 x_train_data = one_hot_encoding(train_data)
 y_train_label = keras.utils.np_utils.to_categorical(train_label)
